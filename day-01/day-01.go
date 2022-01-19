ade4bcbc5bd0054feb222edccf8cf0d08b961121
Keydose <michaelkelly@myself.com>
2022-01-19 19:03:36 +0000

    Working day 1 part 1

--- day-01/day-01.go
+++ day-01/day-01.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 )
 
 func main() {
@@ -15,11 +16,30 @@ func main() {
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
+
+	var increasedCount int = 0
+	var previousValue int = 0
+	var firstValue bool = true
 	for scanner.Scan() {
-		fmt.Println(scanner.Text())
+		currentValue, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if !firstValue {
+			if currentValue > previousValue {
+				increasedCount++
+			}
+		} else {
+			firstValue = false
+		}
+
+		previousValue = currentValue
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
+
+	fmt.Println(increasedCount)
 }

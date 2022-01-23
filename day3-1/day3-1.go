0bb55a8e6c51bb64b27e90a468d3e9720ffc2e6b
Keydose <michaelkelly@myself.com>
2022-01-23 21:57:43 +0000

    Added some comments

--- day3-1/day3-1.go
+++ day3-1/day3-1.go
@@ -22,6 +22,8 @@ func main() {
 	for scanner.Scan() {
 		diagnosticBits := []rune(scanner.Text())
 		if diagnosticCount == 0 {
+			// First diagnostic - create a slice with matching length to store
+			// sum of occurrences of '1' in each place
 			diagnosticLength = len(diagnosticBits)
 			diagnosticBitSums = make([]int, diagnosticLength)
 		}
@@ -40,17 +42,21 @@ func main() {
 
 	for i := 0; i < diagnosticLength; i++ {
 		if diagnosticBitSums[i] > diagnosticCount/2 {
+			// 1 is the most common in i'th place
 			gammaRateBits[i] = '1'
 			epsilonRateBits[i] = '0'
 		} else {
+			// 0 is the most common in i'th place
 			gammaRateBits[i] = '0'
 			epsilonRateBits[i] = '1'
 		}
 	}
 
+	// Convert rune slices into strings
 	gammaRateBinary := string(gammaRateBits)
 	epsilonRateBinary := string(epsilonRateBits)
 
+	// Convert binary strings into decimal unsigned integers
 	gammaRate, _ := strconv.ParseUint(gammaRateBinary, 2, 64)
 	epsilonRate, _ := strconv.ParseUint(epsilonRateBinary, 2, 64)
 

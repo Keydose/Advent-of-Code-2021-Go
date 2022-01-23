2966806683b37a64b43668ec7cbdb9be7dfa3611
Keydose <michaelkelly@myself.com>
2022-01-23 21:54:57 +0000

    First try baby!

---
+++ day3-1/day3-1.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func main() {
+	file, err := os.Open("diagnostic_report.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	diagnosticCount := 0
+	diagnosticLength := 0
+	var diagnosticBitSums []int
+	for scanner.Scan() {
+		diagnosticBits := []rune(scanner.Text())
+		if diagnosticCount == 0 {
+			diagnosticLength = len(diagnosticBits)
+			diagnosticBitSums = make([]int, diagnosticLength)
+		}
+
+		for i := 0; i < diagnosticLength; i++ {
+			if diagnosticBits[i] == '1' {
+				diagnosticBitSums[i]++
+			}
+		}
+
+		diagnosticCount++
+	}
+
+	gammaRateBits := make([]rune, diagnosticLength)
+	epsilonRateBits := make([]rune, diagnosticLength)
+
+	for i := 0; i < diagnosticLength; i++ {
+		if diagnosticBitSums[i] > diagnosticCount/2 {
+			gammaRateBits[i] = '1'
+			epsilonRateBits[i] = '0'
+		} else {
+			gammaRateBits[i] = '0'
+			epsilonRateBits[i] = '1'
+		}
+	}
+
+	gammaRateBinary := string(gammaRateBits)
+	epsilonRateBinary := string(epsilonRateBits)
+
+	gammaRate, _ := strconv.ParseUint(gammaRateBinary, 2, 64)
+	epsilonRate, _ := strconv.ParseUint(epsilonRateBinary, 2, 64)
+
+	powerConsumption := gammaRate * epsilonRate
+
+	fmt.Println(powerConsumption)
+}
